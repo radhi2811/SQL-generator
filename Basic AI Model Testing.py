a6b34ef0d48cf6c3fb2f35e6de0f0d2be86d2d26
radhi2811 <radhinanthini833@gmail.com>
2026-03-09 15:12:36 +0530

    Delete Basic AI Model Testing.py

--- Basic AI Model Testing.py
+++
@@ -1,8 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="ibm-granite/granite-3.3-2b-instruct")
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe(messages)
